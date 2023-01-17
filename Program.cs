0f6cadd2c276295aecb1115e943dc987bafd36c9
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-17 21:01:49 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое значение: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье значение: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine($"Максимальное значение = {a}");
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine($"Максимальное значение = {b}");
+}
+else if (c > a && c > b)
+{
+    Console.WriteLine($"Максимальное значение = {c}");
+}
+else if (a == b && b == c)
+{
+    Console.WriteLine($"Введенв равнозначные значения");
+}
